7853005b3b6110aad1cc35a32a8c43b0df6c9c78
Pedro Franch Echaniz <pedrofe.av@gmail.com>
2023-10-19 14:26:34 +0200

    tabla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MaterialImportModule } from './material-import/material-import.module';
 import { AppComponent } from './app.component';
+import { TablaAlumnoComponent } from './tabla-alumno/tabla-alumno.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TablaAlumnoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/alumno.interface.ts
@@ -0,0 +1,6 @@
+export interface Alumno {
+    name: string;
+    position: number;
+    weight: number;
+    symbol: string;
+}
